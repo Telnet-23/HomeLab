c7c1c31d0db49d3526ef3ce201a9ee91f24d7242
Terry <122405584+Telnet-23@users.noreply.github.com>
2026-08-13 16:30:55 +0100

    Add ArgoCD deployment script

---
+++ ArgoCD/Argo Deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Create the namespace
+kubectl create namespace argocd
+
+# Deploy ArgoCD
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+# Verify the install
+kubectl get all -n argocd
