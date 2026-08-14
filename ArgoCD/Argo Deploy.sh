1ced1b699b1a842b4be88cef56a1ba5913b069b8
Terry <122405584+Telnet-23@users.noreply.github.com>
2026-08-14 15:04:40 +0100

    Enable server-side apply for ArgoCD deployment

--- ArgoCD/Argo Deploy.sh
+++ ArgoCD/Argo Deploy.sh
@@ -4,7 +4,7 @@
 kubectl create namespace argocd
 
 # Deploy ArgoCD
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl apply  -n argocd --server-side -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
 # Verify the install
 kubectl get all -n argocd
